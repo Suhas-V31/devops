489b379921f2cc48d6061cce171391e46ef3dba3
suhas v <suhasv147@gmail.com>
2021-10-28 15:56:32 +0530

    commit

--- add.java
+++ add.java
@@ -2,7 +2,7 @@ public class SumOfNumbers1
 {  
 public static void main(String args[])   
 {  
-int n1 = 05, n2 = 10, sum;  
+int n1 = 205, n2 = 150, sum;  
 sum = n1 + n2;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
